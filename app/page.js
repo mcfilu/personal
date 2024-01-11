f58c53cab0ae9e5155e87bae5534cb7c6c60cc14
mcfilu <filip.1olszewski@gmail.com>
2024-01-11 09:08:50 +0100

    migrating the main component from page.js to its own component

--- app/page.js
+++ app/page.js
@@ -9,37 +9,7 @@ const navigation = [
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      <nav className="my-16 animate-fade-in">
-        <ul className="flex items-center justify-center gap-4">
-          {navigation.map((item) => (
-            <Link
-              key={item.href}
-              href={item.href}
-              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
-            >
-              {item.name}
-            </Link>
-          ))}
-        </ul>
-      </nav>
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      {/* <Particles
-        className="absolute inset-0 -z-10 animate-fade-in"
-        quantity={100}
-      /> */}
-      <div className={`absolute inset-0 -z-10  bg-black bg-opacity-85`}></div>
-      <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        Filip Olszewski
-      </h1>
-
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
-        Not Your Average Software Engineer
-        </h2>
-      </div>
-    </div>
+    
   );
 
 }
