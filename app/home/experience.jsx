8f51ec8163a06ef45b3b6458616b322a7c5bf63f
mcfilu <filip.1olszewski@gmail.com>
2024-01-19 10:40:52 +0100

    making some part of experience comp responsive

--- app/home/experience.jsx
+++ app/home/experience.jsx
@@ -2,21 +2,22 @@ import React from 'react'
 
 const Experience = () => {
   return (
-    <div className='w-full flex flex-col bg-white px-[10vw] '>
+    <div className='w-full flex flex-col bg-white px-[5vw] md:px-[10vw] '>
         <h2 className='text-[7vh] md:text-[8vh] leading-tight font-display text-black mt-[5vh] mb-[5vh] text-center'>My Experience</h2>
         <div className='w-full  flex flex-row justify-center'>
-            <div className='w-1/5 h-full mt-[2vh] '>October 2021 - Current</div>
+            <div className='hidden md:flex w-1/5 h-full mt-[2vh] '>October 2021 - Current</div>
             {/* <div className='h-full border'></div> */}
             <div class="flex flex-col justify-center items-center">
                 <i class="fa-solid fa-circle my-[2vh]"></i>
                 <div class="h-full border-black border-l-2"></div>
             </div>
-            <div className='w-[70%] h-full flex flex-col mt-[2vh] pl-[2vw]'>
+            <div className='w-full md:w-[70%] h-full flex flex-col mt-[2vh] pl-[2vw]'>
                 <h2 className='text-xl bold'>Full Stack Developer</h2>
+                <p>October 2021 - Current</p>
                 <h2 className=''>Papagrin</h2>
                 <p className='mt-[3vh]'>Took charge of a legacy project from the previous IT contractor and resurrected the application without any documentation or runtime environment. Despite two companies giving up on the project, I adeptly established fully functional environments, incorporating Docker containerisation, and implementing CI/CD processes.</p>
                 <p className='mt-[1vh]'>Took charge of a legacy project from the previous IT contractor and resurrected the application without any documentation or runtime environment. Despite two companies giving up on the project, I adeptly established fully functional environments, incorporating Docker containerisation, and implementing CI/CD processes.</p>
-                <div className='w-full grid grid-cols-2 gap-4 mt-[1vh]'>
+                <div className='w-full flex flex-wrap gap-2 md:gap-4 mt-[2vh]'>
                     <div className='bg-black text-white rounded-xl px-4 py-2'>Python</div>
                     <div className='bg-black text-white rounded-xl px-4 py-2'>Django</div>
                     <div className='bg-black text-white rounded-xl px-4 py-2'>Unix</div>
--- app/home/skills.jsx
+++ app/home/skills.jsx
@@ -7,12 +7,12 @@ const Skills = () => {
         <div className='w-full grid grid-cols-1 md:grid-cols-3 h-full'>
         <div className='flex flex-col h-full'>
             <div className='bg-white hover:bg-zinc-100 h-full flex items-center justify-center'>
-                <div className='flex flex-col w-full h-full p-[5vh] items-left justify-center'>
+                <div className='flex flex-col w-full h-full px-[2vw] py-[8vh] items-left justify-center'>
                     <h2 className='text-3xl font-display'>01. Back End</h2>
-                    <p className='mt-[2vh]'>Understand the client’s subject matter completely and build on the knowledge related to it, becoming intimately familiar with the subject matter. Determine methods to address the specific customer’s needs and requirements.</p>
+                    <p className='mt-[4vh]'>Understand the client’s subject matter completely and build on the knowledge related to it, becoming intimately familiar with the subject matter. Determine methods to address the specific customer’s needs and requirements.</p>
                 </div>
             </div>
-            <div className='w-full h-full flex flex-col p-8 py-[5vh] hover:bg-zinc-600 bg-zinc-800'>
+            <div className='w-full h-full flex flex-col px-[2vw] py-[5vh] hover:bg-zinc-600 bg-zinc-800'>
                 <div className='flex flex-row gap-4'>
 
                     <i class='cib-python text-white text-[7vh]'></i>
@@ -34,12 +34,12 @@ const Skills = () => {
         </div>
         <div className='flex flex-col h-full'>
             <div className='bg-white hover:bg-zinc-100 h-full flex items-center justify-center border-x'>
-                <div className='flex flex-col w-full h-full p-[5vh] items-left justify-center'>
+                <div className='flex flex-col w-full h-full px-[2vw] py-[8vh] items-left justify-center'>
                     <h2 className='text-3xl font-display'>02. Front End</h2>
-                    <p className='mt-[2vh]'>Understand the client’s subject matter completely and build on the knowledge related to it, becoming intimately familiar with the subject matter. Determine methods to address the specific customer’s needs and requirements.</p>
+                    <p className='mt-[4vh]'>Understand the client’s subject matter completely and build on the knowledge related to it, becoming intimately familiar with the subject matter. Determine methods to address the specific customer’s needs and requirements.</p>
                 </div>
             </div>
-            <div className='w-full h-full flex flex-col p-8 py-[5vh] hover:bg-zinc-600 bg-zinc-800 border-x'>
+            <div className='w-full h-full flex flex-col px-[2vw] py-[5vh] hover:bg-zinc-600 bg-zinc-800 border-x'>
                 <div className='flex flex-row gap-4'>
 
                     <i class='cib-python text-white text-[7vh]'></i>
@@ -61,12 +61,12 @@ const Skills = () => {
         </div>
         <div className='flex flex-col h-full'>
             <div className='bg-white hover:bg-zinc-100 h-full flex items-center justify-center'>
-                <div className='flex flex-col w-full h-full p-[5vh] items-left justify-center'>
-                    <h2 className='text-3xl font-display'>01. Back End</h2>
-                    <p className='mt-[2vh]'>Understand the client’s subject matter completely and build on the knowledge related to it, becoming intimately familiar with the subject matter. Determine methods to address the specific customer’s needs and requirements.</p>
+                <div className='flex flex-col w-full h-full px-[2vw] py-[8vh] items-left justify-center'>
+                    <h2 className='text-3xl font-display'>03. Other Tools</h2>
+                    <p className='mt-[4vh]'>Understand the client’s subject matter completely and build on the knowledge related to it, becoming intimately familiar with the subject matter. Determine methods to address the specific customer’s needs and requirements.</p>
                 </div>
             </div>
-            <div className='w-full h-full flex flex-col p-8 py-[5vh] hover:bg-zinc-600 bg-zinc-800'>
+            <div className='w-full h-full flex flex-col p-8 px-[2vw] py-[5vh] hover:bg-zinc-600 bg-zinc-800'>
                 <div className='flex flex-row gap-4'>
 
                     <i class='cib-python text-white text-[7vh]'></i>
@@ -91,13 +91,13 @@ const Skills = () => {
         {/* <div className='bg-white hover:bg-zinc-100 h-full flex items-center justify-center border-r border-l'>
             <div className='flex flex-col w-full h-full p-[5vh] items-left justify-center'>
                 <h2 className='text-3xl font-display'>02. Front End</h2>
-                <p className='mt-[2vh] '>Understand the client’s subject matter completely and build on the knowledge related to it, becoming intimately familiar with the subject matter. Determine methods to address the specific customer’s needs and requirements.</p>
+                <p className='mt-[4vh] '>Understand the client’s subject matter completely and build on the knowledge related to it, becoming intimately familiar with the subject matter. Determine methods to address the specific customer’s needs and requirements.</p>
             </div>
         </div>
         <div className='bg-white hover:bg-zinc-100 h-full flex items-center justify-center'>
             <div className='flex flex-col w-full h-full p-[5vh] items-left justify-center'>
                 <h2 className='text-3xl font-display'>03. Infrastracture</h2>
-                <p className='mt-[2vh]'>Understand the client’s subject matter completely and build on the knowledge related to it, becoming intimately familiar with the subject matter. Determine methods to address the specific customer’s needs and requirements.</p>
+                <p className='mt-[4vh]'>Understand the client’s subject matter completely and build on the knowledge related to it, becoming intimately familiar with the subject matter. Determine methods to address the specific customer’s needs and requirements.</p>
             </div>
         </div> */}
         </div>
