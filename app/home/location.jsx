b18293e97f9c33b560a40381b32b4f94b293df41
mcfilu <filip.1olszewski@gmail.com>
2024-02-12 13:16:45 +0100

    fixing error by adding ) in location component

--- app/home/location.jsx
+++ app/home/location.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Location = () => {
   return (
-    <div className='w-full flex flex-col md:flex-row bg-white py-[5vh]'>
+    <div className='w-full flex flex-col md:flex-row bg-white py-[10vh]'>
         <div className='w-full md:w-1/2 flex flex-row items-center pl-[5vw]'>
             <h2 className='text-[7vh] md:text-[8vh] leading-tight text-black font-display text-center md:text-left'>Based In London</h2>
             {/* <h2 className='text-[7vh] md:text-[8vh] leading-tight text-black'>In</h2> */}
@@ -13,6 +13,7 @@ const Location = () => {
             <p className='mt-[2vh] text-[1.7vh]'>I am a seasoned software expert who has developed products for enterprise clients such as Intuit, Yale University, and Hewlett-Packard (HP), as well as many venture-backed businesses. I specialize in building applications specific to the business needs of my clients. I have done work in software development, mobile app creation, front-end/back-end web, database/server management, graphic design, and video game development.</p>
         </div>
     </div>
+  )
 }
 
 export default Location
