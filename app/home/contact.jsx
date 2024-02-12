bec10dc4a92e8a992ff3f0d544412905e5a493bf
mcfilu <filip.1olszewski@gmail.com>
2024-02-12 15:23:34 +0100

    changing the size of the icons in the contact section

--- app/home/contact.jsx
+++ app/home/contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
             {/* <span className='w-[1px] h-full bg-white relative left-20'></span> */}
             <div className='flex flex-col items-center justify-center h-full'>
                 <div className='w-[20vh] h-[20vh] rounded-full border flex items-center justify-center'>
-                    <i className={`fa-brands fa-github text-center text-[15vh] text-white`}></i>
+                    <i className={`fa-brands fa-github text-center text-[12vh] text-white`}></i>
                 </div>
                 <span className='text-3xl text-white mt-[5vh]'>McFilu</span>
                 <span className='text-l text-white mt-[2vh]'>GitHub</span>
@@ -21,7 +21,7 @@ const Contact = () => {
             {/* <span className='w-[1px] h-full bg-white relative left-20'></span> */}
             <div className='flex flex-col items-center justify-center h-full'>
                 <div className='w-[20vh] h-[20vh] rounded-full border flex items-center justify-center'>
-                    <i className={`fa-solid fa-envelope text-center text-[15vh] text-white`}></i>
+                    <i className={`fa-solid fa-envelope text-center text-[12vh] text-white`}></i>
                 </div>
                 <span className='text-3xl text-white mt-[5vh]'>filip.1olszewski@gmail.com</span>
                 <span className='text-l text-white mt-[2vh]'>Email</span>
@@ -31,7 +31,7 @@ const Contact = () => {
             {/* <span className='w-[1px] h-full bg-white relative left-20'></span> */}
             <div className='flex flex-col items-center justify-center h-full'>
                 <div className='w-[20vh] h-[20vh] rounded-full border flex items-center justify-center'>
-                    <i className={`fa-brands fa-linkedin text-center text-[15vh] text-white`}></i>
+                    <i className={`fa-brands fa-linkedin text-center text-[12vh] text-white`}></i>
                 </div>
                 <span className='text-3xl text-white mt-[5vh]'>Filip1Olszewski</span>
                 <span className='text-l text-white mt-[2vh]'>Linkedin</span>
