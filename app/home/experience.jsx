3780a7865635c194649b5f8a650f64b039335e8f
mcfilu <filip.1olszewski@gmail.com>
2024-02-12 13:14:40 +0100

    changing the experience header

--- app/home/experience.jsx
+++ app/home/experience.jsx
@@ -29,7 +29,7 @@ const Experience = () => {
         </div>
 
         <div className='w-full  flex flex-row justify-center mt-[5vh]'>
-            <div className='hidden md:flex w-1/5 h-full mt-[2vh] '>October 2021 - Current</div>
+            <div className='hidden md:flex w-1/5 h-full mt-[2vh] '>December 2023 - Current</div>
             {/* <div className='h-full border'></div> */}
             <div class="flex flex-col justify-center items-center">
                 <i class="fa-solid fa-circle my-[3vh]"></i>
@@ -37,7 +37,7 @@ const Experience = () => {
             </div>
             <div className='w-full md:w-[70%] h-full flex flex-col mt-[2vh] pl-[2vw]'>
                 <h2 className='text-[3vh] font-bold'>Research Python Developer</h2>
-                <p className='flex md:hidden'>October 2021 - Current</p>
+                <p className='flex md:hidden'>December 2023 - Current</p>
                 <h2 className=''>Cracow University of Economics</h2>
                 <p className='mt-[3vh] text-[1.7vh]'>Took charge of a legacy project from the previous IT contractor and resurrected the application without any documentation or runtime environment. Despite two companies giving up on the project, I adeptly established fully functional environments, incorporating Docker containerisation, and implementing CI/CD processes.</p>
                 <p className='mt-[1vh] text-[1.7vh]'>Took charge of a legacy project from the previous IT contractor and resurrected the application without any documentation or runtime environment. Despite two companies giving up on the project, I adeptly established fully functional environments, incorporating Docker containerisation, and implementing CI/CD processes.</p>
@@ -53,7 +53,7 @@ const Experience = () => {
         </div>
 
         <div className='w-full  flex flex-row justify-center mt-[5vh]'>
-            <div className='hidden md:flex w-1/5 h-full mt-[2vh] '>October 2021 - Current</div>
+            <div className='hidden md:flex w-1/5 h-full mt-[2vh] '>December 2023 - Current</div>
             {/* <div className='h-full border'></div> */}
             <div class="flex flex-col justify-center items-center">
                 <i class="fa-solid fa-circle my-[3vh]"></i>
